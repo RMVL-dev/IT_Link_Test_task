f3b5f7d1ab6ae2be13eebca08b35f6b60452dcbb
ramilmavlihanov <Ramil040201@gmail.com>
2024-12-24 13:53:51 +0300

    ADD: cache image, hide/unhide tool bar, load image

--- app/src/main/java/com/example/itlinktesttask/ui/imageScreen/ImageFragment.kt
+++ app/src/main/java/com/example/itlinktesttask/ui/imageScreen/ImageFragment.kt
@@ -1,10 +1,21 @@
 package com.example.itlinktesttask.ui.imageScreen
 
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.example.itlinktesttask.R
 import com.example.itlinktesttask.databinding.FragmentImageBinding
 
 class ImageFragment : Fragment() {
@@ -24,7 +35,40 @@ class ImageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.test.text = arguments?.getString(BUNDLE_ID)
+
+        val brokenImage =
+            ResourcesCompat.getDrawable(requireContext().resources, R.drawable.ic_broken_image, requireContext().theme)
+
+        val placeHolder =
+            ResourcesCompat.getDrawable(requireContext().resources, R.drawable.ic_image_placeholder, requireContext().theme)
+
+        if (requireContext().resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES) {
+            brokenImage?.let { DrawableCompat.setTint(it, Color.WHITE) }
+            placeHolder?.let { DrawableCompat.setTint(it, Color.WHITE) }
+        }
+
+        val options: RequestOptions = RequestOptions()
+            .dontAnimate()
+            .centerInside()
+            .placeholder(placeHolder) //Use a placeholder image
+            .error(brokenImage)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .priority(Priority.HIGH)
+
+        Glide.with(binding.ivImage)
+            .load(arguments?.getString(BUNDLE_ID))
+            .apply(options)
+            .into(binding.ivImage)
+
+        binding.ibBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.ivImage.setOnClickListener {
+            binding.toolbar.isVisible = !binding.toolbar.isVisible
+            binding.divider.isVisible = !binding.divider.isVisible
+        }
+
     }
 
     companion object{
